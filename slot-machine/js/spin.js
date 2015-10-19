ed95cf9f35c20b543b8b2b55dd30bb5f83d2f2ff
phanhuy1502 <phanhuy1502@gmail.com>
2015-10-19 14:27:09 +0800

    change spin.html UI

--- slot-machine/js/spin.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('#arm').click(function(e) {
-		var arm = $(this).addClass('clicked'),
-			delay = setTimeout(function() { arm.removeClass('clicked') }, 500);
-		e.preventDefault();
-		spin();
-	});
-});
